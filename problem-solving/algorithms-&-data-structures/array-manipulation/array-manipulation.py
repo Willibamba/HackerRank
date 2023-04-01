8401ed32352c748dbc1200144668b0e96301f556
Momodou Williams <williamsmomodou@gmail.com>
2023-04-01 05:46:21 +0300

    Added data array-manipulation solutions and problem statement

---
+++ problem-solving/algorithms-&-data-structures/array-manipulation/array-manipulation.py
@@ -0,0 +1,59 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+import itertools
+
+#
+# Complete the 'arrayManipulation' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY queries
+#
+
+def arrayManipulation(n, queries):
+    # Write your code here
+    index = [0] * n
+    
+    for query in queries:
+        left = query[0]
+        right = query[1]
+        increment = query[2]
+        
+        index[left-1] -= increment
+        if right < n:
+            index[right] -= increment
+
+    count = 0
+    maximum = 0
+    for j in range(n):
+        count += index[j]
+        if count > maximum:
+            maximum = count
+            
+    return maximum     
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    queries = []
+
+    for _ in range(m):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    result = arrayManipulation(n, queries)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
