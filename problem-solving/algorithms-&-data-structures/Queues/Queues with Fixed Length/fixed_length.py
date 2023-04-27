647d615065f064303a6fa5c89b3195b31d775336
Momodou Williams <williamsmomodou@gmail.com>
2023-04-27 05:56:56 +0300

    Added problem statement and code solution for 'Queues with Fixed Length'

---
+++ problem-solving/algorithms-&-data-structures/Queues/Queues with Fixed Length/fixed_length.py
@@ -0,0 +1,62 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'solve' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY arr
+#  2. INTEGER_ARRAY queries
+#
+
+def solve(arr, queries):
+    # Write your code here
+   
+    minimums = []
+    
+    for query in queries:
+        
+        minimum = max(arr[:query])
+        i = 1
+        while i <= len(arr)-query:
+            
+            subarrays = max(arr[i:i+query])
+            if subarrays < minimum:
+                
+                minimum = subarrays
+            
+            i += 1
+        
+        minimums.append(minimum)
+        
+    return minimums
+   
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    q = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    queries = []
+
+    for _ in range(q):
+        queries_item = int(input().strip())
+        queries.append(queries_item)
+
+    result = solve(arr, queries)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
