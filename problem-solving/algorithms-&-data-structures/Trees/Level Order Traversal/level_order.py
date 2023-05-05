88cd443bedb7ba908bf4cbe56706e2e6e123b468
Momodou Williams <williamsmomodou@gmail.com>
2023-05-05 05:22:30 +0300

    Added the code solution for 'Tree: Level Order Traversal'

---
+++ problem-solving/algorithms-&-data-structures/Trees/Level Order Traversal/level_order.py
@@ -0,0 +1,72 @@
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.info (the value of the node)
+"""
+def levelOrder(root):
+    #Write your code here
+    if not root:
+        return
+    # Enque root the queue
+    queue = [root]
+    
+    # Loop through and deque the first element while queue is not empty
+    # Print the the value
+    while len(queue) > 0:
+        root = queue.pop(0)
+        print(root.info, end=" ")
+        
+        # If any of the condition is true, enque to the end of queue
+        if root.left:
+            queue.append(root.left)
+        if root.right:
+            queue.append(root.right)
+        
+
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+levelOrder(tree.root)
